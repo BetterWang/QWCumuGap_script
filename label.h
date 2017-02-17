320d034c6536f334cddf7c251042eef56235efab
Quan Wang <BetterWang@gmail.com>
2017-02-17 06:29:32 +0100

    [+] label

--- label.h
+++ label.h
@@ -1,10 +1,26 @@
 
 char const * fname[] = {
 	"test/",			// 0
+	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_v1/170215_180405/0000/",			// 1 pPb HM0 standard
+	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_v1/170215_180420/0000/",			// 2 pPb HM1 standard
+	"",			// 3 pPb HM2 standard
+	"",			// 4 pPb HM3 standard
+	"",			// 5 pPb HM4 standard
+	"",			// 6 pPb HM5 standard
+	"",			// 7 pPb HM6 standard
+	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_v1/170215_180432/0000/",			// 8 pPb HM7 standard
 };
 
 char const * ftxt[] = {
 	"txt/test/",			// 0
+	"txt/PAHM0/",			// 1
+	"txt/PAHM1/",			// 2
+	"txt/PAHM2/",			// 3
+	"txt/PAHM3/",			// 4
+	"txt/PAHM4/",			// 5
+	"txt/PAHM5/",			// 6
+	"txt/PAHM6/",			// 7
+	"txt/PAHM7/",			// 8
 };
 
 
