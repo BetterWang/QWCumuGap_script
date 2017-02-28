b01692b00e03cc759da8dc43a248acf3f4a167a7
Quan Wang <BetterWang@gmail.com>
2017-02-28 08:11:46 +0100

    [!] label

--- label.h
+++ label.h
@@ -1,20 +1,20 @@
 
 char const * fname[] = {
 	"test/",			// 0
-	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_v3/170220_042427/0000/",			// 1 pPb HM0 standard
-	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_v3/170220_042439/0000/",			// 2 pPb HM1 standard
+	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_v4/170227_152330/0000/",			// 1 pPb HM0 standard
+	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_v4/170227_152351/0000/",			// 2 pPb HM1 standard
 	"",			// 3 pPb HM2 standard
 	"",			// 4 pPb HM3 standard
 	"",			// 5 pPb HM4 standard
 	"",			// 6 pPb HM5 standard
 	"",			// 7 pPb HM6 standard
-	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_v3/170220_042451/0000/",			// 8 pPb HM7 standard
-	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_v3/170220_042255/0000/",				// 9 pPb MB1 standard
-	"../pPb/PAMinimumBias2/crab_PAMB2_cumugap_eff_v3/170220_042306/0000/",				// 10 pPb MB1 standard
-	"../pPb/PAMinimumBias3/crab_PAMB3_cumugap_eff_v3/170220_042321/0000/",				// 11 pPb MB1 standard
-	"../pPb/PAMinimumBias4/crab_PAMB4_cumugap_eff_v3/170220_042334/0000/",				// 12 pPb MB1 standard
-	"../pPb/PAMinimumBias5/crab_PAMB5_cumugap_eff_v3/170220_042346/0000/",				// 13 pPb MB1 standard
-	"../pPb/PAMinimumBias6/crab_PAMB6_cumugap_eff_v3/170220_042359/0000/",				// 14 pPb MB1 standard
+	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_v4/170227_152418/0000/",			// 8 pPb HM7 standard
+	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_v4/170222_233431/0000/",				// 9 pPb MB1 standard
+	"../pPb/PAMinimumBias2/crab_PAMB2_cumugap_eff_v4/170227_152727/0000/",				// 10 pPb MB2 standard
+	"../pPb/PAMinimumBias3/crab_PAMB3_cumugap_eff_v4/170227_152748/0000/",				// 11 pPb MB3 standard
+	"../pPb/PAMinimumBias4/crab_PAMB4_cumugap_eff_v4/170227_152827/0000/",				// 12 pPb MB4 standard
+	"../pPb/PAMinimumBias5/crab_PAMB5_cumugap_eff_v4/170227_152848/0000/",				// 13 pPb MB5 standard
+	"../pPb/PAMinimumBias6/crab_PAMB6_cumugap_eff_v4/170227_152904/0000/",				// 14 pPb MB6 standard
 };
 
 char const * ftxt[] = {
