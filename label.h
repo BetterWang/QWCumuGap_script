c83a82616e5bb3b4df81936a3c6e10b3f3f4c937
Quan Wang <BetterWang@gmail.com>
2017-03-06 12:15:36 +0100

    [+] label

--- label.h
+++ label.h
@@ -1,36 +1,38 @@
 
 char const * fname[] = {
 	"test/",			// 0
-	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_v4/170227_152330/0000/",			// 1 pPb HM0 standard
-	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_v4/170227_152351/0000/",			// 2 pPb HM1 standard
+	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_v5/170303_135919/0000/",			// 1 pPb HM0 standard
+	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_v5/170304_100734/0000/",			// 2 pPb HM1 standard
 	"",			// 3 pPb HM2 standard
 	"",			// 4 pPb HM3 standard
 	"",			// 5 pPb HM4 standard
 	"",			// 6 pPb HM5 standard
 	"",			// 7 pPb HM6 standard
-	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_v4/170227_152418/0000/",			// 8 pPb HM7 standard
-	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_v4/170222_233431/0000/",				// 9 pPb MB1 standard
+	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_v5/170304_100748/0000/",			// 8 pPb HM7 standard
+	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_v5/170304_101121/0000/",				// 9 pPb MB1 standard
 	"../pPb/PAMinimumBias2/crab_PAMB2_cumugap_eff_v4/170227_152727/0000/",				// 10 pPb MB2 standard
 	"../pPb/PAMinimumBias3/crab_PAMB3_cumugap_eff_v4/170227_152748/0000/",				// 11 pPb MB3 standard
 	"../pPb/PAMinimumBias4/crab_PAMB4_cumugap_eff_v4/170227_152827/0000/",				// 12 pPb MB4 standard
 	"../pPb/PAMinimumBias5/crab_PAMB5_cumugap_eff_v4/170227_152848/0000/",				// 13 pPb MB5 standard
 	"../pPb/PAMinimumBias6/crab_PAMB6_cumugap_eff_v4/170227_152904/0000/",				// 14 pPb MB6 standard
         "NA",			// 15 pPb MB sum standard
-	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_reverse_v4/170227_155556/0000/",		// 16 pPb HM0 reverse
-	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_reverse_v4/170227_155622/0000/",		// 17 pPb HM1 reverse
+	"../pPb/PAHighMultiplicity0/crab_PAHM0_cumugap_eff_reverse_v5/170305_142853/0000/",		// 16 pPb HM0 reverse
+	"../pPb/PAHighMultiplicity1/crab_PAHM1_cumugap_eff_reverse_v5/170305_142915/0000/",		// 17 pPb HM1 reverse
 	"",			// 18 pPb HM2 reverse
 	"",			// 19 pPb HM3 reverse
 	"",			// 20 pPb HM4 reverse
 	"",			// 21 pPb HM5 reverse
 	"",			// 22 pPb HM6 reverse
-	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_reverse_v4/170227_155653/0000/",		// 23 pPb HM7 reverse
-	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_reverse_v4/170301_110704/0000/",			// 24 pPb MB1 reverse
-	"../pPb/PAMinimumBias2/crab_PAMB2_cumugap_eff_reverse_v4/170301_110720/0000/",			// 25 pPb MB2 reverse
-	"../pPb/PAMinimumBias3/crab_PAMB3_cumugap_eff_reverse_v4/170301_110737/0000/",			// 26 pPb MB3 reverse
-	"../pPb/PAMinimumBias4/crab_PAMB4_cumugap_eff_reverse_v4/170301_110758/0000/",			// 27 pPb MB4 reverse
-	"../pPb/PAMinimumBias5/crab_PAMB5_cumugap_eff_reverse_v4/170301_110816/0000/",			// 28 pPb MB5 reverse
-	"../pPb/PAMinimumBias6/crab_PAMB6_cumugap_eff_reverse_v4/170301_110832/0000/",			// 29 pPb MB6 reverse
+	"../pPb/PAHighMultiplicity7/crab_PAHM7_cumugap_eff_reverse_v5/170305_142933/0000/",		// 23 pPb HM7 reverse
+	"../pPb/PAMinimumBias1/crab_PAMB1_cumugap_eff_reverse_v5/170305_143849/0000/",			// 24 pPb MB1 reverse
+	"",			// 25 pPb MB2 reverse
+	"",			// 26 pPb MB3 reverse
+	"",			// 27 pPb MB4 reverse
+	"",			// 28 pPb MB5 reverse
+	"",			// 29 pPb MB6 reverse
 	"NA",		// 30 pPb MB sum reverse
+	"../pPb/HighMultiplicity/crab_PPHM_totem_cumugap_eff_v1/170303_103746/0000/",			// 31 pp HM TOTEM
+	"../PbPb2015_cumu/HIMinimumBias5/crab_HIMB5_CumuGap_ppReco_eff_noff_v1/170303_115252/0000/",	// 32 PbPb 5 TeV pp reco
 };
 
 char const * ftxt[] = {
@@ -65,6 +67,8 @@ char const * ftxt[] = {
 	"txt/PAMB5_reverse/",		// 28
 	"txt/PAMB6_reverse/",		// 29
 	"txt/PAMB_reverse_full/",	// 30
+	"txt/PP15_TOTEM/",		// 31
+	"txt/PbPb15_ppreco/",		// 32
 };
 
 
