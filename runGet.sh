4037e925d99842c892f29d48b87b56b22788b762
Quan Wang <BetterWang@gmail.com>
2017-05-17 10:08:00 +0200

    [+] update

--- runGet.sh
+++ runGet.sh
@@ -1,14 +1,100 @@
+./runGet3.sh 61 0 10  
+./runGet3.sh 61 1 10  
+./runGet3.sh 61 2 10  
+./runGet3.sh 61 3 10  
+./runGet3.sh 61 4 10  
+./runGet3.sh 61 5 10  
+./runGet3.sh 61 6 10  
+./runGet3.sh 61 7 10  
+./runGet3.sh 61 8 10  
+./runGet3.sh 61 9 10  
+./runGet3.sh 61 10 10 
 
-./runGet3.sh 161 0 10  
-./runGet3.sh 161 1 10  
-./runGet3.sh 161 2 10  
-./runGet3.sh 161 3 10  
-./runGet3.sh 161 4 10  
-./runGet3.sh 161 5 10  
-./runGet3.sh 161 6 10  
-./runGet3.sh 161 7 10  
-./runGet3.sh 161 8 10  
-./runGet3.sh 161 9 10  
-./runGet3.sh 161 10 10 
+./runGet3.sh 62 0 10  
+./runGet3.sh 62 1 10  
+./runGet3.sh 62 2 10  
+./runGet3.sh 62 3 10  
+./runGet3.sh 62 4 10  
+./runGet3.sh 62 5 10  
+./runGet3.sh 62 6 10  
+./runGet3.sh 62 7 10  
+./runGet3.sh 62 8 10  
+./runGet3.sh 62 9 10  
+./runGet3.sh 62 10 10 
 
 
+
+
+
+
+./runGet3.sh 63 0 10  
+./runGet3.sh 63 1 10  
+./runGet3.sh 63 2 10  
+./runGet3.sh 63 3 10  
+./runGet3.sh 63 4 10  
+./runGet3.sh 63 5 10  
+./runGet3.sh 63 6 10  
+./runGet3.sh 63 7 10  
+./runGet3.sh 63 8 10  
+./runGet3.sh 63 9 10  
+./runGet3.sh 63 10 10 
+
+./runGet3.sh 64 0 10  
+./runGet3.sh 64 1 10  
+./runGet3.sh 64 2 10  
+./runGet3.sh 64 3 10  
+./runGet3.sh 64 4 10  
+./runGet3.sh 64 5 10  
+./runGet3.sh 64 6 10  
+./runGet3.sh 64 7 10  
+./runGet3.sh 64 8 10  
+./runGet3.sh 64 9 10  
+./runGet3.sh 64 10 10 
+
+./runGet3.sh 65 0 10  
+./runGet3.sh 65 1 10  
+./runGet3.sh 65 2 10  
+./runGet3.sh 65 3 10  
+./runGet3.sh 65 4 10  
+./runGet3.sh 65 5 10  
+./runGet3.sh 65 6 10  
+./runGet3.sh 65 7 10  
+./runGet3.sh 65 8 10  
+./runGet3.sh 65 9 10  
+./runGet3.sh 65 10 10 
+
+./runGet3.sh 66 0 10  
+./runGet3.sh 66 1 10  
+./runGet3.sh 66 2 10  
+./runGet3.sh 66 3 10  
+./runGet3.sh 66 4 10  
+./runGet3.sh 66 5 10  
+./runGet3.sh 66 6 10  
+./runGet3.sh 66 7 10  
+./runGet3.sh 66 8 10  
+./runGet3.sh 66 9 10  
+./runGet3.sh 66 10 10 
+
+./runGet3.sh 67 0 10  
+./runGet3.sh 67 1 10  
+./runGet3.sh 67 2 10  
+./runGet3.sh 67 3 10  
+./runGet3.sh 67 4 10  
+./runGet3.sh 67 5 10  
+./runGet3.sh 67 6 10  
+./runGet3.sh 67 7 10  
+./runGet3.sh 67 8 10  
+./runGet3.sh 67 9 10  
+./runGet3.sh 67 10 10 
+
+./runGet3.sh 68 0 10  
+./runGet3.sh 68 1 10  
+./runGet3.sh 68 2 10  
+./runGet3.sh 68 3 10  
+./runGet3.sh 68 4 10  
+./runGet3.sh 68 5 10  
+./runGet3.sh 68 6 10  
+./runGet3.sh 68 7 10  
+./runGet3.sh 68 8 10  
+./runGet3.sh 68 9 10  
+./runGet3.sh 68 10 10 
--- runGetE.sh
+++ runGetE.sh
@@ -1,1 +1,8 @@
-./runGetE3.sh 161
+./runGetE3.sh 61
+./runGetE3.sh 62
+./runGetE3.sh 63
+./runGetE3.sh 64
+./runGetE3.sh 65
+./runGetE3.sh 66
+./runGetE3.sh 67
+./runGetE3.sh 68
